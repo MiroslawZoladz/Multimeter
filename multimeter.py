f4180d5517aab08164329b06340f8d69ada4ddba
Miro <zoladz@agh.edu.pl>
2022-07-02 10:32:40 +0200

    multimeter driver as class

---
+++ multimeter.py
@@ -0,0 +1,38 @@
+import serial
+
+class Multimeter:
+    def __init__(self,com_nr):
+        self.com_nr = com_nr
+        self.check_id()
+        
+    def commmand(self,cmd):
+       
+        comm = serial.Serial(f'Com{self.com_nr}',115200,timeout=1)
+        comm.flushInput()
+        
+        comm.write(cmd.encode('UTF-8')+b'\r\n')
+        comm.flushOutput()
+        
+        comm.readline()
+        res = comm.readline()
+        
+        comm.close()
+        
+        return res    
+    
+    def check_id(self):
+        res = self.commmand('id').strip()
+        assert res == b'multimeter', 'multimeter module not found'
+    
+    def ai(self,ch_nr=''):
+        res = self.commmand(f'i {ch_nr}')
+        return  list(map(float,res.split()[:-1]))
+    
+    def ao(self,out0, out1):
+        self.commmand(f'o {out0} {out1}')
+
+
+mm = Multimeter(56)
+mm.ao(0.123, 0.321)
+print(mm.ai())
+
